852e56d34d33235b32e1f529a3fae367ea342a82
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2022-08-11 00:23:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Rendez());
+}
+
+class Rendez extends StatelessWidget {
+  const Rendez({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
